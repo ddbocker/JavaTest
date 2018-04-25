9c458c14b25f2f802c1beb4bd05a0a2d0204de77
cjh <cjh861125@126,com>
2018-04-25 16:51:07 +0800

    spring boot init

---
+++ src/main/java/com/cjh/cisdi/test/tinywebapplication/TinywebapplicationApplication.java
@@ -0,0 +1,12 @@
+package com.cjh.cisdi.test.tinywebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinywebapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinywebapplicationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cjh/cisdi/test/tinywebapplication/TinywebapplicationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cjh.cisdi.test.tinywebapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TinywebapplicationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
